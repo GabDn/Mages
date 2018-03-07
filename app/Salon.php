9283467ca907da5714750b86676b881284318301
GerardoArjona <gerardo.arj15@gmail.com>
2018-03-07 11:37:31 -0600

    Update Salon.php

--- app/Salon.php
+++ app/Salon.php
@@ -6,17 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Salon extends Model
 {
-    protected $table = 'salons';
-
-       /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-       'sede','capacidad','ubicacion','sigla'
-    ];
-
 
 
 }
