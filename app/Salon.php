069bb25bf9449124930685c80a305e7a0e3bdb73
GerardoArjona <gerardo.arj15@gmail.com>
2018-03-07 10:45:52 -0600

    Add files via upload
    Se completo el modelo se Salon

--- app/Salon.php
+++ app/Salon.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Salon extends Model
 {
-    //
+    protected $table = 'salons';
+
+       /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+       'sede','capacidad','ubicacion','sigla'
+    ];
+
+
+
 }
